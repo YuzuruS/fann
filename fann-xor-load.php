82de405c1530af5fd9b56cc78756965e9d790d0e
YuzuruS <navitima@gmail.com>
2016-07-12 10:29:14 +0900

    first commit

---
+++ fann-xor-load.php
@@ -0,0 +1,8 @@
+<?php
+$ann = fann_create_from_file('fann-xor.net');
+
+$xor_pattern = [[0,0], [0,1], [1,0], [1,1]];
+foreach ($xor_pattern as $x) {
+  $r = fann_run($ann, $x);
+  printf("%d %d => %d\n", $x[0], $x[1], round($r[0]));
+}
